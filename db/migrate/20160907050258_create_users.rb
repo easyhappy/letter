a146eef2db7a2c217ef5b852660facff735797dd
AndyHu <meeasyhappy@gmail.com>
2016-09-07 13:34:14 +0800

    创建user 表

--- db/migrate/20160907050258_create_users.rb
+++ db/migrate/20160907050258_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :username, null: false, unique: true
-      t.string :password, null: false
       t.timestamps
     end
   end
