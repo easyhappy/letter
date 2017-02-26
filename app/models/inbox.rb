d20d8b3fe6084dfdfcd72acb54f3d7f25c4b8f4b
andyHu <meeasyhappy@gmail.com>
2017-02-26 13:40:15 +0800

    No message.

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -2,6 +2,7 @@ class Inbox < ApplicationRecord
   include Common::SoftDelete
   belongs_to :user
   belongs_to :friend, class_name: "User"
+  has_many  :messages
 
   def reset_unread_count
     self.update_columns(:unread_count => 0)
